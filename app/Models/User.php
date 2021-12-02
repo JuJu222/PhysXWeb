7e4c797a68cfcd68a01fd8831785083c8ef78be6
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-02 23:04:52 +0700

    Added fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'school',
+        'city',
+        'birthyear',
+        'role'
     ];
 
     /**
