6a195d8092cbec5bfb79ed1da27ae87309af946c
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-04 23:17:25 +0700

    Sidebar update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -32,9 +32,9 @@
           </svg>
           <span class="visually-hidden">Icon-only</span>
         </a>
-        <ul class="nav nav-pills nav-flush mt-5 flex-column text-center">
+        <ul class="nav nav-pills nav-flush flex-column text-center">
           <li class="nav-item">
-            <a href="#" class="nav-link py-3 mt-5" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+            <a href="#" class="nav-link py-3" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
               <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#6F69AC" class="bi bi-house-door" viewBox="0 0 16 16">
                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
               </svg>
