197739fa7e3ef6e37519f7032c927a5ef5fd84b1
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2021-12-06 21:34:41 +0700

    Deleted twitter bootstrap

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,7 +9,6 @@
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
