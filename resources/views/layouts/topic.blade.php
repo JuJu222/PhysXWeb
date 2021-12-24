a34ebba1e169efb3b77873716a3e817f32158783
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-24 19:27:32 +0700

    Besaran Dan Satuan

--- resources/views/layouts/topic.blade.php
+++ resources/views/layouts/topic.blade.php
@@ -7,7 +7,9 @@
 <div class="d-flex justify-content-center mt-5 me-5">
     <span><h2 class="mt-3 fw-bold purple">{{ $topic->topic_name }}</h2></span>     
 </div>
+<div class="mt-5 me-5">
 @yield('topic_content')
+</div>
 <div class="d-flex justify-content-center mt-5 me-5">
     <button type="button" class="btn btn-primary btn-lg mx-5">Mudah</button>
     <button type="button" class="btn btn-primary btn-lg mx-5">Susah</button>     
