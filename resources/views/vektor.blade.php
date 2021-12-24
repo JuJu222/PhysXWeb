b4215826f7bae38824cd7d72cdfc2d27dd2ad90d
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-24 20:05:47 +0700

    Vektor

--- resources/views/vektor.blade.php
+++ resources/views/vektor.blade.php
@@ -1,3 +1,10 @@
 @extends('layouts.topic')
 @section('topic_content')
+<p>Besaran dapat memiliki arah dan tidak.</p>
+<p>Besaran yang memiliki arah disebut sebagai besaran <span class="fw-bolder">vektor</span>.</p>
+<p>Sedangkan, besaran yang tidak memiliki arah disebut sebagai besaran <span class="fw-bolder">skalar</span>.</p>
+<p>Sehingga, besaran skalar hanya memiliki nilai dan selalu positif, namun besaran vektor adalah besaran yang mempunyai nilai dan arah.</p>
+<br/>
+<p>Simak video yang dibuat oleh <span class="fw-bolder">Quipper</span> terkait vektor untuk dapat mengerti materi ini lebih dalam.</p>
+<iframe width="560" height="315" class="rounded mx-auto d-block" src="https://www.youtube.com/embed/OJrffDuVXjM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 @endsection
