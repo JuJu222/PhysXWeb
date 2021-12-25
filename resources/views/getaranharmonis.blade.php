55979dec0052a04d85fbabd3907be89289fae7c1
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-25 17:47:06 +0700

    Topic Finished

--- resources/views/getaranharmonis.blade.php
+++ resources/views/getaranharmonis.blade.php
@@ -1,3 +1,66 @@
 @extends('layouts.topic')
 @section('topic_content')
+<p>Getaran Harmonis adalah sebuah benda yang bergerak secara bolak balik (periodik) melalui titik kesetimbangan. Grafik letak partikel ini diartikan sebagai fungsi waktu yang berupa sinus dinyatakan dalam bentuk sinus dan kosinus. Gerak ini juga sering dinamakan sebagai gerak osilasi.</p>
+<p>Gambaran atau contoh dari sistem yang menggunakan prinsip getaran harmonis sendiri misalnya seperti, dawai pada alat musik, gelombang radio, arus listrik AC dan denyut jantung.</p>
+<br/>
+<h3>Syarat-syarat Getaran Harmonis</h3>
+<p>1. Sistem Gerakannya secara periodik atau bolak-balik.</p>
+<p>2. Proses Gerakannya akan selalu melewati kedudukan keseimbangan.</p>
+<p>3. Kemudian pada Percepatan atau gaya yang bekerja yang terdapat pada sebuah benda akan dapat sebanding dengan kedudukan atau simpangan benda.</p>
+<p>4. Kemudian Arah dalam percepatan atau gaya yang bekerja yang ada didalam suatu benda selalu mengarah kedudukan keseimbangan.</p>
+<br/>
+<p>Gerak harmonik sederhana memiliki simpangan, kecepatan, percepatan, dan energi.</p>
+<br/>
+<h3>Simpangan</h3>
+<br/>
+<p>Simpangan getaran harmonik sederhana dapat dianggap sebagai proyeksi partikel yang bergerak melingkar beraturan pada diameter lingkaran.</p>
+<figure>
+    <img src="https://live.staticflickr.com/65535/49750780716_b7352559b3_z.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<p>y = simpangan getaran (m)</p>
+<p>ω = kecepatan sudut (rad/s)</p>
+<p>T = periode (s)</p>
+<p>f = frekuensi (Hz)</p>
+<p>t = waktu tempuh (s)</p>
+<p>A = amplitudo/simpangan maksimum (m)</p>
+<br/>
+<h3>Kecepatan</h3>
+<br/>
+<p>Kecepatan merupakan turunan pertama dari posisi. Pada gerak harmonik sederhana, kecepatan diperoleh dari turunan pertama persamaan simpangan. Persamaan kecepatan dapat dijabarkan sebagai berikut.</p>
+<figure>
+    <img src="https://live.staticflickr.com/65535/49750780851_70b9bc60e0_z.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<h3>Percepatan</h3>
+<p>Percepatan benda yang bergerak harmonik sederhana dapat diperoleh dari turunan pertama persamaan kecepatan atau turunan kedua persamaan simpangan. Persamaan percepatan dapat diperoleh sebagai berikut.</p>
+<figure>
+    <img src="https://live.staticflickr.com/65535/49750241518_9cace830d7_z.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<p>Simpangan maksimum memiliki nilai yang sama dengan amplitudo (y = A), sehingga percepatan maksimumnya adalah am= – Aw</p>
+<h3>Energi</h3>
+<p>Persamaan energi pada gerak harmonik sederhana meliputi energi kinetik, energi potensial, dan energi mekanik.</p>
+<p>Energi kinetik benda dapat dirumuskan sebagai berikut.</p>
+<figure>
+    <img src="https://www.flickr.com/photos/185371494@N06/49751109447/in/dateposted-public/" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<p>Energi potensial benda dapat dirumuskan sebagai berikut.</p>
+<figure>
+    <img src="https://live.staticflickr.com/65535/49751109517_d8d047c826.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<p>Sementara itu, energi mekanik adalah penjumlahan dari energi kinetik dan energi potensial.</p>
+<figure>
+    <img src="https://live.staticflickr.com/65535/49750241633_bc35ff3266_z.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kelas Pintar</figcaption>
+</figure>
+<p>k = nilai ketetapan (N/m)</p>
+<p>ω = kecepatan sudut (rad/s)</p>
+<p>A = amplitudo (m)</p>
+<p>t = waktu tempuh (s)</p>
+<p>Jumlah energi potensial dan energi kinetik benda yang bergerak harmonik sederhana selalu bernilai tetap.</p>
+<p>Simak <span class="fw-bolder">video</span> yang dibuat oleh <span class="fw-bolder">GIA Academy</span> terkait getaran harmonis untuk dapat mengerti materi ini lebih dalam.</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/E07LwrQQb30" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 @endsection
