9d86146e05e0ff7200ad978f14f9643e574df15f
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-25 15:49:20 +0700

    Parabola

--- resources/views/gerakparabola.blade.php
+++ resources/views/gerakparabola.blade.php
@@ -1,3 +1,38 @@
 @extends('layouts.topic')
 @section('topic_content')
+<p>Gerak parabola adalah <span class="fw-bolder">gabungan antara gerak lurus beraturan (GLB) dan gerak lurus berubah beraturan (GLBB)</span>.</p>
+<p class="fw-bolder">Gerak parabola merupakan gerak dua dimensi suatu benda yang bergerak membentuk sudut elevasi dengan sumbu x atau sumbu y. </p>
+<p>Sumbu x (horizontal) merupakan GLB dan sumbu y (vertikal) merupakan GLBB.</p>
+<p>Kedua gerak ini tidak saling memengaruhi, hanya saja membentuk suatu gerak parabola.</p>
+<p>Berikut adalah contoh gerakan parabola.</p>
+<figure>
+    <img src="https://asset.kompas.com/data/photo/2020/10/02/5f7733cb9dfdc.png" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Kompas</figcaption>
+</figure>
+<p class="fw-bolder">Dalam gerak parabola, komponen sumbu x merupakan komponen GLB.</p>
+<p>GLB merupakan kecepatan di sumbu horizontal pada titik ataupun posisi tetap.</p>
+<br/>
+<h3>Sumbu X</h3>
+<p>Pada sumbu x, komponen awal ialah simbol dari kecepatan awal.</p>
+<p>Secara matematis, nilai didapatkan dengan persamaan di bawah ini:</p>
+<figure>
+    <img src="https://www.ruangguru.com/hs-fs/hubfs/Rumus%201-2.jpg?width=600&name=Rumus%201-2.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Ruang Guru</figcaption>
+</figure>
+<p>Perhatikan juga tabel di bawah ini untuk referensi:</p>
+<figure>
+    <img src="https://www.ruangguru.com/hs-fs/hubfs/tabel%20gerak%20parabola.jpg?width=600&name=tabel%20gerak%20parabola.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Ruang Guru</figcaption>
+</figure>
+<h3>Sumbu Y</h3>
+<p class="fw-bolder">Jika sumbu x merupakan komponen GLB, sumbu y atau arah vertikal komponen gerak merupakan GLBB.</p>
+<p>Perbedaan sumbu x dengan sumbu y ialah simbol perpindahan/jarak pada sumbu x ditunjukkan dengan s, sedangkan pada sumbu y ditunjukkan dengan y.</p>
+<p>Sumbu y kecepatan awal disimbolkan dengan. Sehingga, dapat dirumuskan sebagai berikut:</p>
+<figure>
+    <img src="https://www.ruangguru.com/hs-fs/hubfs/Rumus%202-3.jpg?width=600&name=Rumus%202-3.jpg" class="rounded mx-auto d-block">
+    <figcaption class="text-center">Sumber: Ruang Guru</figcaption>
+</figure>
+<p>Simak video yang dibuat oleh <span class="fw-bolder">Quipper</span> terkait vektor untuk dapat mengerti materi ini lebih dalam.</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/mk1j0qqhT7E" title="YouTube video player" frameborder="0" class="rounded mx-auto d-block" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p>Sumber: Ruang Guru</p>
 @endsection
