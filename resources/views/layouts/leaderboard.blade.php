4b10e4f362b3f5150fb709f0f63c73e9722800b2
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-27 21:17:26 +0700

    Enhanced leaderboard layout

--- resources/views/layouts/leaderboard.blade.php
+++ resources/views/layouts/leaderboard.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.apps')
 @section('content')
+<div class="d-flex justify-content-end mt-5 me-5">
+    <span><h5 class="mt-3 fw-bold purple">Justin Jap</h5></span>
+    <img src="https://github.com/mdo.png" alt="mdo" class="rounded-circle ms-3" width="60" height="60">       
+</div>
+<div class="d-flex justify-content-center mt-5 me-5">
+    <span><h2 class="mt-3 fw-bold purple">Nama Topik</h2></span>     
+</div>
+<div class="mt-5 me-5">
+@yield('leaderboard_content')
+</div>
 @endsection
