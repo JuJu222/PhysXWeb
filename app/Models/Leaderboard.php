209d683fb0d9909eef12bc78e58449060bd34e1c
Kenny Jinhiro Wibowogit <kenny.jinhiro@gmail.com>
2021-12-27 21:47:46 +0700

    Model done

--- app/Models/Leaderboard.php
+++ app/Models/Leaderboard.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Leaderboard extends Model
 {
     use HasFactory;
+    protected $fillable = ['id_leaderboard','uid','id_topic','score'];
+    public function fis10user(){
+        return $this->belongsTo(Fis10User::class,'uid', 'fis10_user_id');
+    }
+
+    public function topic(){
+        return $this->belongsTo(Topic::class, 'id_topic', 'topic_id');
+    }
 }
