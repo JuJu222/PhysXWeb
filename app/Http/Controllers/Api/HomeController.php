56a2a080c719ad6e9202c39b1d5856df59d0444f
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2022-01-04 23:01:10 +0700

    Updated home api

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Fis10User;
+use App\Models\UsersQuestions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,13 +15,35 @@ class HomeController extends Controller
         $fis10user = Fis10User::query()->where('user_id', Auth::id())->first();
         $name = $fis10user->user->name;
         $unlockedTopics = $fis10user->topics;
-        $totalScore = 0;
-        $ranking = 0;
+        $userTitle = null;
+        $userAvatar = null;
+        $onwedItems = $fis10user->shopItem;
         $coins = $fis10user->coins;
 
-        $fis10UserQuestions = $fis10user->questions;
-        foreach ($fis10UserQuestions as $fis10UserQuestion) {
-            $totalScore += $fis10UserQuestion->pivot->question_score;
+        $rankings = UsersQuestions::selectRaw("CAST(SUM(question_score) AS INTEGER) AS total_score, fis10_user_id")
+            ->groupBy('fis10_user_id')
+            ->orderByDesc('total_score')
+            ->get();
+
+        $totalScore = '0';
+        $ranking = '-';
+        foreach ($rankings as $index=>$item) {
+            if ($item->fis10_user_id == $fis10user->fis10_user_id) {
+                $totalScore = $item->total_score;
+                $ranking = $index + 1;
+            }
+        }
+
+        if ($onwedItems != null) {
+            foreach ($onwedItems as $onwedItem) {
+                if ($onwedItem->pivot->is_equipped) {
+                    if ($onwedItem['type'] == 'title') {
+                        $userTitle = $onwedItem['item'];
+                    } else {
+                        $userAvatar = $onwedItem['image_path'];
+                    }
+                }
+            }
         }
 
         $response['name'] = $name;
@@ -28,6 +51,8 @@ class HomeController extends Controller
         $response['total_score'] = $totalScore;
         $response['ranking'] = $ranking;
         $response['coins'] = $coins;
+        $response['title'] = $userTitle;
+        $response['avatar'] = $userAvatar;
 
         return $response;
     }
