e75bd114500981dd8258551311d88009a4b8e278
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2022-01-05 14:51:10 +0700

    Changed topic button

--- resources/views/layouts/topic.blade.php
+++ resources/views/layouts/topic.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.apps')
 @section('content')
-<div class="d-flex justify-content-center mt-5 me-5">
+<div class="d-flex justify-content-center mt-5">
     <span><h2 class="mt-3 fw-bold purple">{{ $topic->topic_name }}</h2></span>
 </div>
 <div class="mt-5 me-5">
 @yield('topic_content')
 </div>
-<div class="d-flex justify-content-center mt-5 me-5">
+<div class="d-flex justify-content-center mt-5">
     <h3 class="fw-bolder">Mari tantang diri anda dan dapatkan skor yang terbaik!</h3>
 </div>
-<div class="d-flex justify-content-center mt-5 me-5">
-    <a href="{{ route('questionSoal', $topicsEasy) }}" class="btn btn-primary btn-lg mx-5" type="button">Mudah</a>
-    <a href="{{ route('questionSoal', $topicsHard) }}" class="btn btn-primary btn-lg mx-5 "type="button">Susah</a>
+<div class="d-flex justify-content-center mt-3 mb-5">
+    <a href="{{ route('questionSoal', $topicsEasy) }}" class="btn btn-opaque rounded-circleX px-4 mx-3" type="button">Mudah</a>
+    <a href="{{ route('questionSoal', $topicsHard) }}" class="btn btn-opaque rounded-circleX px-4 mx-3" type="button">Susah</a>
 </div>
 @endsection
